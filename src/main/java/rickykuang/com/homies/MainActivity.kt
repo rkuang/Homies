57cb2aa3e90bf35ed8dc982ac7820fb659a5a62f
Ricky Kuang <rkuang26@gmail.com>
2018-08-17 14:35:33 -0700

    added menu option for user sign out

--- src/main/java/rickykuang/com/homies/MainActivity.kt
+++ src/main/java/rickykuang/com/homies/MainActivity.kt
@@ -1,13 +1,23 @@
 package rickykuang.com.homies
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     private lateinit var mAuth: FirebaseAuth
 
@@ -18,14 +28,35 @@ class MainActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance();
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater;
+        inflater.inflate(R.menu.my_menu, menu)
+        return true
+    }
+
     override fun onStart() {
         super.onStart()
 
         var currentUser: FirebaseUser? = mAuth.currentUser;
-        Log.d("MainActivity", "Logged in as: $currentUser")
+        Log.d(TAG, "Logged in as: ${currentUser?.displayName}")
 
         if (currentUser == null) {
-            
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.signout -> {
+                AuthUI.getInstance()
+                        .signOut(this).addOnCompleteListener( OnCompleteListener {
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        })
+                return true
+            }
+            else -> return true
         }
     }
 }
