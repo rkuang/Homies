c3dded1513b1239a2dd61fd7d2eef112f851a6e5
Ricky Kuang <rkuang26@gmail.com>
2018-09-05 01:34:27 -0700

    empty message warning for chat

--- app/src/main/java/com/rickykuang/homies/activities/main/fragments/MessagesFragment.kt
+++ app/src/main/java/com/rickykuang/homies/activities/main/fragments/MessagesFragment.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import com.rickykuang.homies.Homies
 import com.rickykuang.homies.R
 import com.rickykuang.homies.activities.main.adapters.MessagesAdapter
 import com.rickykuang.homies.models.Message
 import com.rickykuang.homies.utils.FirestoreUtil
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import kotlin.math.roundToInt
 
 
 class MessagesFragment : Fragment() {
@@ -56,12 +59,17 @@ class MessagesFragment : Fragment() {
 
     private fun sendButtonClickListener(v: View) {
         val edit_message = v.findViewById<EditText>(R.id.edit_message)
-        if (edit_message.text.isNotEmpty()) {
+        val message_text = edit_message.text.toString().trim()
+        if (message_text.isNotEmpty()) {
             val currentUser = Homies.mAuth.currentUser!!
-            val message = Message(currentUser.uid, currentUser.displayName!!, edit_message.text.toString(), null)
+            val message = Message(currentUser.uid, currentUser.displayName!!, message_text, null)
             FirestoreUtil.addMessage(message)
-            edit_message.text.clear()
+        } else {
+            val toast = Toast.makeText(context, R.string.message_empty, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, Math.max(0, edit_message.y.roundToInt() + toast.yOffset))
+            toast.show()
         }
+        edit_message.text.clear()
     }
 
     private fun initAdapter() : MessagesAdapter {
